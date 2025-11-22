c906908deca1c5527f056402a338563a3ec1e4d3
Eli <eli7serrano@gmail.com>
2025-11-22 12:33:41 -0600

    Implementa la clase Constantes que contenga las constantes matemáticas más comunes (PI, E, etc.). Implementa la clase Constantes que contenga las constantes matemáticas más comunes (PI, E, etc.).

--- src/secuenciasdeescape.java
+++
@@ -1,8 +0,0 @@
-public class secuenciasdeescape {
-    public static void main(String[] args){
-        String texto = "C:L\\Hola \"Mundo\">"; //escape con comillas dobles
-        String text = "Hola \n\t Mundo"; // salto de linea \n y tabulacion \t
-        System.out.println(texto);
-        System.out.println(text);
-    }
-}
